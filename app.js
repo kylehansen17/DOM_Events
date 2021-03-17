35e3e9e5217d945d146c705fc456cff992760796
kylehansen17 <kyle.hansen925@gmail.com>
2021-03-17 07:50:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -36,3 +36,39 @@ const tasButton = document.querySelector('#tas');
 tasButton.addEventListener('click', twist)
 tasButton.addEventListener('click', shout)
 // will execute both functions
+
+
+// Assuming there is a page with 50+ buttons
+const makeRandColor = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+const buttons = document.querySelectorAll('button');
+
+for (let button of buttons) {
+    button.addEventListener('click', function () {
+        button.style.backgroundColor = makeRandColor();
+        button.style.color = makeRandColor();
+    })
+}
+// Using THIS to make reusable function out of above function
+function colorize() {
+    this.style.backgroundColor = makeRandColor();
+    this.style.color = makeRandColor();
+}
+// Now updating the button function
+for (let button of buttons) {
+    button.addEventListener('click', colorize)
+}
+
+
+// Event Object
+window.addEventListener('keydown', function (e) {
+    console.log(e.code)
+})
+// The above will log any key that is pressed when on the page
+// e.key = end result
+// e.code = physical location of what is pressed
